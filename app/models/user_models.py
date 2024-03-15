87bc5af8b6b80e4b74102889f5331188ee9bbe1f
geoffkeyrock <geoff@porchlink.com>
2024-03-15 12:46:29 -0600

    pwlib

--- app/models/user_models.py
+++ app/models/user_models.py
@@ -19,7 +19,7 @@ class User(UserMixin, db.Model):
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password, 'passlib')
+        self.password_hash = generate_password_hash(password, 'PBKDF2')
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
